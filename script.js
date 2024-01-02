442a27c19f68cca7e8668d4cbae7a76eca58c35d
lmcosta10 <letmc14@gmail.com>
2024-01-02 15:04:44 -0300

    Generalize clickAnimation(button)

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 
 const btnConvert = document.querySelector(".convert");
 
-const clickConvertAnimation = function () {
-  btnConvert.style.backgroundColor = "#f8f8f8";
-  btnConvert.style.transform = "translate(0.1rem, 0.1rem)";
+const clickAnimation = function (button) {
+  button.style.backgroundColor = "#f8f8f8";
+  button.style.transform = "translate(0.1rem, 0.1rem)";
   setTimeout(function () {
-    btnConvert.style.transform = "translate(0, 0)";
-    btnConvert.style.backgroundColor = "#fff";
+    button.style.transform = "translate(0, 0)";
+    button.style.backgroundColor = "#fff";
   }, 100);
 };
 
 const convert = function () {
-  clickConvertAnimation();
+  clickAnimation(btnConvert);
 };
 
 btnConvert.addEventListener("click", convert);
